95df89f6ad50a8493a131b2a5e7bb35dc194d05c
william lebouvier <wlebouvier@normandiewebschool.fr>
2023-11-13 10:37:38 +0100

    v2.4

--- Admin.php
+++ Admin.php
@@ -1,17 +1,15 @@
 <?php include_once 'templates/headerAdmin.html'; ?>
 
-<h1>nom bio stock prix image</h1>
-
-    <form class="product-form" method="POST" action="php/productCreate.php" enctype="multipart/form-data">
-        <label for="pname" class="form-label">Nom du produit :</label>
-        <input type="text" id="pname" name="pname" class="form-input" required><br>
-        <label for="pprice" class="form-label">Prix du produit :</label>
-        <input type="text" id="pprice" name="pprice" class="form-input" required><br>
-        <label for="pdesc" class="form-label">Description du produit :</label>
-        <input type="text" id="pdesc" name="pdesc" class="form-input" required><br>
-        <label for="pstock" class="form-label">Stock du produit :</label>
-        <input type="text" id="pstock" name="pstock" class="form-input" required><br>
-        <label for="pimage" class="form-label">Image du produit :</label>
-        <input type="file" id="pimage" name="pimage" class="image" accept="image/*"><br>
-        <input type="submit" value="Mettre en ligne le produit" class="submit-button">
-    </form>
+<form class="product-form" method="POST" action="php/productCreate.php" enctype="multipart/form-data">
+    <label for="nom" class="form-label">Nom du produit :</label>
+    <input type="text" id="nom" name="nom" class="form-input" required><br>
+    <label for="prix" class="form-label">Prix du produit :</label>
+    <input type="text" id="prix" name="prix" class="form-input" required><br>
+    <label for="bio" class="form-label">Description du produit :</label>
+    <input type="text" id="bio" name="bio" class="form-input" required><br>
+    <label for="stock" class="form-label">Stock du produit :</label>
+    <input type="text" id="stock" name="stock" class="form-input" required><br>
+    <label for="image" class="form-label">Image du produit :</label>
+    <input type="file" id="image" name="image" class="image" accept="image/*"><br>
+    <input type="submit" value="Mettre en ligne le produit" class="submit-button">
+</form>
--- php/productCreate.php
+++ php/productCreate.php
@@ -5,22 +5,28 @@ $pdoManager = new DBManagement("market-nws");
 $pdo = $pdoManager->getPDO();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $pnom = $_POST["pnom"];
-    $pprix = $_POST["pprix"];
-    $pbio = $_POST["pbio"];
-    $pstock = $_POST["pstock"];
+    $nom = $_POST["nom"];
+    $prix = $_POST["prix"];
+    $bio = $_POST["bio"];
+    $stock = $_POST["stock"];
 
-    if (isset($_FILES["pimage"]) && $_FILES["pimage"]["error"] == 0) {
-        $imageData = file_get_contents($_FILES["pimage"]["tmp_non"]);
-        $imageType = $_FILES["pimage"]["type"];
+    if (isset($_FILES["image"]) && $_FILES["image"]["error"] == 0) {
+        $imageData = file_get_contents($_FILES["image"]["tmp_name"]);
+        $imageType = $_FILES["image"]["type"];
     } else {
         echo "Erreur : Veuillez sélectionner une image valide.";
         exit;
     }
 
-    $sql = "INSERT INTO products (nom, prix, bio, stock, image_data, image_type) VALUES (?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO produit (nom, prix, bio, stock, image_data, image_type) VALUES (?, ?, ?, ?, ?, ?)";
     $stmt = $pdo->prepare($sql);
-    $stmt->bind_param("ssssbs", $pnom, $pprix, $pbio, $pstock, $imageData, $imageType);
+    // $stmt->bind_param("ssssbs", $nom, $prix, $bio, $stock, $imageData, $imageType);
+    $stmt->bindParam(1, $nom, PDO::PARAM_STR);
+    $stmt->bindParam(2, $prix, PDO::PARAM_STR);
+    $stmt->bindParam(3, $bio, PDO::PARAM_STR);
+    $stmt->bindParam(4, $stock, PDO::PARAM_STR);
+    $stmt->bindParam(5, $imageData, PDO::PARAM_LOB);
+    $stmt->bindParam(6, $imageType, PDO::PARAM_STR);
 
     if ($stmt->execute()) {
         echo "Le produit a été ajouté avec succès.";
