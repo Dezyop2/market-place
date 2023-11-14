4894c0cf05ff21eb7109b5c6c65d944aefd0405c
william lebouvier <wlebouvier@normandiewebschool.fr>
2023-11-14 11:03:32 +0100

    v3

--- index.php
+++ index.php
@@ -10,21 +10,25 @@ require_once 'php/connexion.php';
 $pdoManager = new DBManagement("market-nws");
 $pdo = $pdoManager->getPDO();
 
-$sql = "SELECT * FROM produit ORDER BY stock ASC LIMIT 1";
+$sql = "SELECT * FROM produit ORDER BY stock ASC LIMIT 4";
 $stmt = $pdo->query($sql);
 
 if ($stmt) {
-    $product = $stmt->fetch(PDO::FETCH_ASSOC);
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    if ($product) {
-        echo '<div class="product-container">';
-        echo '<div class="product-details">';
-        echo "<h2 class='product-title'>" . $product["nom"] . "</h2>";
-        echo '<img src="data:' . $product["image_type"] . ';base64,' . base64_encode($product["image_data"]) . '" alt="' . $product["nom"] . '" class="product-image">';
-        echo '<div class="product-info">';
-        echo "<p class='product-price'><a class='titre'>prix : </a>" . $product["prix"] . "</p>";
-        echo '</div>';
-        echo '</div>';
+    if ($products) {
+        echo '<div class="products-container">';
+        foreach ($products as $product) {
+            echo '<div class="product-container">';
+            echo '<div class="product-details">';
+            echo "<h2 class='product-title'>" . $product["nom"] . "</h2>";
+            echo '<div class="product-image-container">';
+            echo '<img src="data:' . $product["image_type"] . ';base64,' . base64_encode($product["image_data"]) . '" alt="' . $product["nom"] . '" class="product-image">';
+            echo '</div>';
+            echo "<p class='product-price'><a class='titre'>prix : </a>" . $product["prix"] . "</p>";
+            echo '</div>';
+            echo '</div>';
+        }
         echo '</div>';
     } else {
         echo "Aucun produit trouvé.";
